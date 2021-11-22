07276c0e7aecf1388d66a751a8d714d8d9a0a7e5
Chris Kiilu <cndeti@gmail.com>
2021-11-22 17:22:16 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ce la vis'));
+app.get('/', (req, res) => res.send('This better be the last time'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
